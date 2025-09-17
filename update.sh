77a04c0723e6d55687efbaca2d615adf400c84dd
AZen <418130444@qq.com>
2025-09-17 14:42:09 +0800

    update

---
+++ update.sh
@@ -0,0 +1,10 @@
+
+git add .
+git commit -m "update"
+git push
+
+
+# git rev-list --objects --all | grep  ed7efa83ece36758f1da1eb88667b42bf3cf38fd26bc3f5e844d3f42b3233895
+
+# git filter-branch --tree-filter 'rm -f moka-ai_m3e-base/pytorch_model.bin' --tag-name-filter cat -- --all
+
